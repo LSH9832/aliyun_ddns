fa82be7dc7d2b396945574044f75402f4deb77e7
SHL <69880398+LSH9832@users.noreply.github.com>
2024-10-08 23:49:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,42 +6,104 @@
 import os
 import time
 import urllib.request
-from lxml import etree
+# from lxml import etree
 from aliyunsdkcore.client import AcsClient
 # from aliyunsdkcore.acs_exception.exceptions import ClientException
 # from aliyunsdkcore.acs_exception.exceptions import ServerException
 from datetime import datetime
 from aliyunsdkalidns.request.v20150109 import DescribeSubDomainRecordsRequest, AddDomainRecordRequest, UpdateDomainRecordRequest, DeleteDomainRecordRequest
 import json, urllib
+import yaml
+import os.path as osp
+
+
+CONFIG_FILE = "config.yaml"
+REGION_ID = 'cn-hangzhou'
+TTL = 600
+
+if not osp.isfile(CONFIG_FILE):
+    cfg = {
+        "default": {
+            "regionID": REGION_ID,
+            "ttl": TTL,
+            "id": "your_id",
+            "key": "your_key"
+        },
+        "tasks": [
+            {
+                "domain": "cctv.com",
+                "record_type": "A",
+                "record_value": "www"
+            },
+            {
+                "domain": "cctv.com",
+                "record_type": "AAAA",
+                "record_value": "ipv6.www",
+                "interface": "eth0",
+                "index": 1
+            }
+        ]
+    }
+    yaml.dump(cfg, open(CONFIG_FILE, "w"), yaml.Dumper)
+    print(f'modify {osp.abspath(CONFIG_FILE)} and then restart this code')
+    exit(0)
+else:
+    cfg = yaml.load(open(CONFIG_FILE, "r"), yaml.Loader)
+    REGION_ID = cfg["default"]["regionID"]
+    TTL = cfg["default"]["ttl"]
 
 
 # 获取所属公网ip
-def get_outside_ip():
+def get_outside_ip(*args):
     with urllib.request.urlopen('https://www.3322.org/dyndns/getip') as response:
         ip = (response.read()).decode().rstrip('\n')
     return ip
 
 
+def get_ipv6_ip(now_index=0):
+    interface = cfg["tasks"][now_index]["interface"]
+    idx = cfg["tasks"][now_index]["index"]
+    flag = False
+    count = 0
+    ip_str = ""
+    for line in os.popen("ifconfig").read().split("\n"):
+        
+        if "flags" in line and "mtu" in line:
+            flag = interface in line
+            count = 0
+                
+        elif flag and "inet6" in line:
+            print(line, flag)
+            if count == idx:
+                ip_str = line.split()[1]
+                break
+            count += 1
+    
+    return ip_str
+    
+    
+
 class DDNS(object):
 
-    __regionId = 'cn-hangzhou'
+    __regionId = REGION_ID
     __task = {}
-    __delay = 600
+    __delay = TTL
 
-    def __init__(self, ID, SECRET):
-        self.__ID = ID
-        self.__SECRET = SECRET
-        self.__client = AcsClient(ID, SECRET, self.__regionId)
+    def __init__(self):
+        self.__ID = cfg["default"]["id"]
+        self.__SECRET = cfg["default"]["key"]
+        self.__client = AcsClient(self.__ID, self.__SECRET, self.__regionId)
 
     @staticmethod
-    def __write_to_log(msg):
+    def __write_to_log(msg, write=True):
         log_file_name = 'log.txt'
         if os.path.exists(log_file_name):
             string = open(log_file_name).read()
         else:
             string = ''
         print('[%s] %s' % (str(datetime.now()).split('.')[0], msg))
-        open(log_file_name, 'w').write('%s[%s] %s\n' % (string, str(datetime.now()).split('.')[0], msg))
+        if write:
+            open(log_file_name, 'w').write('%s[%s] %s\n' % (string, str(datetime.now()).split('.')[0], msg))
 
     # 查询记录
     def __getDomainInfo(self, SubDomain):
@@ -68,9 +130,9 @@ class DDNS(object):
         response = str(response, encoding='utf-8')
         return response
 
-    def __ddns_for_one_record(self, DomainName, RR, address_source, msg, address_type='A'):
+    def __ddns_for_one_record(self, DomainName, RR, address_source, msg, address_type='A', idx=0):
         SubDomain = '%s.%s' % (RR, DomainName) if not RR == '@' else DomainName   # 子域名
-        real_address = address_source()                                         # 实际IP
+        real_address = address_source(idx)                                         # 实际IP
 
         for this_record in msg['DomainRecords']['Record']:
             if this_record['Type'] == address_type:
@@ -81,7 +143,7 @@ class DDNS(object):
                     returnmsg = self.__updateDomainRecord(real_address, RR, recordID, address_type)
                     self.__write_to_log('msg of %s from aliyun: %s' % (SubDomain, returnmsg))
                 else:
-                    self.__write_to_log('ip of %s not changed' % SubDomain)
+                    self.__write_to_log('ip of %s not changed' % SubDomain, False)
                 return True
             else:
                 return False
@@ -94,7 +156,8 @@ class DDNS(object):
         for i in range(self.__task[DomainName][RR]['totalCount']):
             address_source = self.__task[DomainName][RR]['address_source'][i]
             address_type = self.__task[DomainName][RR]['address_type'][i]
-            if not self.__ddns_for_one_record(DomainName, RR, address_source, msg, address_type):
+            index = self.__task[DomainName][RR]['index']
+            if not self.__ddns_for_one_record(DomainName, RR, address_source, msg, address_type, index):
                 self.__write_to_log('%s has no record with type of %s' %(DomainName, address_type))
 
     def __ddns_for_once(self):
@@ -105,7 +168,7 @@ class DDNS(object):
 
     ####################################################################################################################
 
-    def add_task(self, DomainName, RR, address_source, address_type):
+    def add_task(self, DomainName, RR, address_source, address_type, idx):
         if DomainName not in self.__task:
             self.__task[DomainName] = {}
         if RR not in self.__task[DomainName]:
@@ -116,6 +179,7 @@ class DDNS(object):
             self.__task[DomainName][RR]['address_source'].append(address_source)
             self.__task[DomainName][RR]['address_type'].append(address_type)
             self.__task[DomainName][RR]['totalCount'] += 1
+            self.__task[DomainName][RR]['index'] = idx
 
     def get_all_task(self):
         return self.__task
@@ -133,25 +197,19 @@ class DDNS(object):
 
 
 if __name__ == '__main__':
-    my_ID = '此处填入你的阿里云的ID'
-    my_SECRET = '此处填入你的阿里云的KEY'
-    
-    # 以下四个列表内容一一对应填入
-    my_DomainNames = ['your_first_website_name.com', 'www.your_second_website_name.com']
-    my_RRs = ['@', 'www']
-    my_ip_sources = [get_outside_ip, get_outside_ip]
-    my_Record_Types = ['A', 'A']
+    my_ddns = DDNS()
 
-    my_ddns = DDNS(ID=my_ID, SECRET=my_SECRET)
-
-    for my_DomainName, my_RR, my_ip_source, my_Record_Type in zip(my_DomainNames, my_RRs, my_ip_sources, my_Record_Types):
-        my_ddns.add_task(DomainName=my_DomainName,                  # 域名
-                         RR=my_RR,                                  # 主机记录
-                         address_source=my_ip_source,               # 记录值来源，返回的一定要是字符串
-                         address_type=my_Record_Type)               # 记录类型
+    for i, v in enumerate(cfg["tasks"]):
+        my_ddns.add_task(
+            DomainName=v["domain"],                                                                     # 域名
+            RR=v["record_value"],                                                                       # 主机记录
+            address_source=get_ipv6_ip if v["record_type"] == "AAAA" else get_outside_ip,               # 记录值来源，返回的一定要是字符串
+            address_type=v["record_type"],                                                              # 记录类型
+            idx=i                                                                                       
+        )               
 
     print(my_ddns.get_all_task())
-    my_ddns.set_delay(minutes=10)                                   # 每10分钟查询一次是否有变化
-    # my_ddns.set_delay(seconds=10)                                 # 每10秒钟查询一次是否有变化
-    # my_ddns.set_delay(hours=10)                                   # 每10小时查询一次是否有变化
-    my_ddns.start_service()                                         # 开始服务
+    my_ddns.set_delay(minutes=TTL / 60)                                     # 每XX分钟查询一次是否有变化
+    # my_ddns.set_delay(seconds=TTL)                                        # 每XX秒钟查询一次是否有变化
+    # my_ddns.set_delay(hours=TTL / 3600)                                   # 每XX小时查询一次是否有变化
+    my_ddns.start_service()                                                 # 开始服务
